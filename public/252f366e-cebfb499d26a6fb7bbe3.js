8594747d209dacd3de844fe78a6042d3dbfe0058
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2021-08-28 14:01:03 -0400

    run build

---
+++ public/252f366e-cebfb499d26a6fb7bbe3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksuperportfolio=self.webpackChunksuperportfolio||[]).push([[228],{2775:function(t,r,u){u.d(r,{I5p:function(){return n}});var e=u(1046);function n(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-cebfb499d26a6fb7bbe3.js.map
