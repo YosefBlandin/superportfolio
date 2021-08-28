a26d8785d2f256e1ade3b9474db0860b53a41597
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2021-08-28 15:23:43 -0400

    fix: project elements disalignment

--- src/components/UI/Card/index.js
+++ src/components/UI/Card/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ProjectDescription } from '../../ProjectDescription';
-import { CardText } from '../CardText';
+import { MdInfo } from "react-icons/md"
 import { card } from './styles.module.scss';
 
 export const Card = ({ imgSrc = "", imgAlt = "project", title = "Weather App", subtitle = "React js" }) => {
     const [showDescription, setShowDescription] = React.useState(false);
 
-    const cardTextProps = { showDescription, setShowDescription, title, subtitle };
-
     return (
         <>
         <article className={card}>
-            <CardText {...cardTextProps}/>
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+            <MdInfo onClick={() => setShowDescription(!showDescription)}/>
         </article>
         {showDescription && (
             <ProjectDescription showDescription={showDescription} setShowDescription={setShowDescription}/>
