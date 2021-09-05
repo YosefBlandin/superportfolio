e4bd6426ceb2a7398ca854faffb08258d6c51f18
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2021-09-04 22:03:35 -0400

    adjustments in media queries

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,9 +1,41 @@
 import React from "react"
+import { Card } from "../../components/UI/Card";
+import { TopProjectsContainer } from "../../components/UI/TopProjectsContainer";
+import { container, regularProjects } from "./styles.module.scss"
 
 
 const projects = () => {
     return (
-        <h1>Projects</h1>
+        <section className={container}>
+          <TopProjectsContainer>
+          <Card 
+          title="Ultra Morty" 
+          subtitle="Redux, React JS, Sass"
+          />
+          <Card
+           title="Blandin's chat"
+           subtitle="Next JS, Socket IO, MongoDB"
+           />
+          <Card 
+          title="Weatherstonishing"
+          subtitle="Next JS, Sass, Openweather API"
+          />
+        </TopProjectsContainer>
+        <div className={regularProjects}>
+        <Card 
+          title="Machete Store" 
+          subtitle="React APIs, Sass, MongoDB"
+          />
+          <Card
+           title="Weather App"
+           subtitle="React App"
+           />
+          <Card 
+            title="Defining"
+            subtitle="Next JS, Sass"
+          />
+        </div>
+        </section>
     )
 }
 
